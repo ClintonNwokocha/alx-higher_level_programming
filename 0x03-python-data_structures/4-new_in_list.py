bf2ffb4efd2a62a037d7aba7f9a4ceab700c8ad3
Clinton Nwokocha <leoclinton2011@hotmail.com>
2024-01-08 13:38:50 -0800

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,8 @@ def new_in_list(my_list, idx, element):
     modified_list = my_list[:]
     if idx < 0:
         return modified_list
-    elif idx >= len(my_list):
+    if idx >= len(my_list):
         return modified_list
-    else:
-        modified_list[idx] = element
-        return modified_list
+    modified_list[idx] = element
+    return modified_list
     

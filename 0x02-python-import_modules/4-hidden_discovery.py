1a1a4c6ea1562cc4abeb5d7a521bfc33af2ebe3c
Clinton Nwokocha <leoclinton2011@hotmail.com>
2024-01-10 04:10:45 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from hidden_4 import *
     all_func = dir()
-    for i in range(0, len(all_func)):
+    for i in range(len(all_func)):
         if all_func[i][:2] != "__":
             print("{:s}".format(all_func[i]))
 

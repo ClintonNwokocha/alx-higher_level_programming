06b04d28c20290926b7a0f00e58dbf616784df24
Clinton Nwokocha <leoclinton2011@hotmail.com>
2024-01-10 02:53:12 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     import sys
     addition = 0
     for i in range(len(sys.argv) - 1):
-        addition +=  int(sys.argv[i + 1])
+        addition += int(sys.argv[i + 1])
     print("{}".format(addition))

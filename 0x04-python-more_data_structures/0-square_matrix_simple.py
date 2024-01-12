bd96e9d6930926e01ea0f0b2cc14f826cfcae7d5
Clinton Nwokocha <leoclinton2011@hotmail.com>
2024-01-12 01:25:20 -0800

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        sq_row = list(map(lambda x : x ** 2, i))
+
+        new_matrix.append(sq_row)
+    return new_matrix

ac266e46bf99530be671ce331a62048de819ca00
Clinton Nwokocha <leoclinton2011@hotmail.com>
2024-01-30 15:23:52 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""A class square that has a private instance attribute"""
+
+class Square:
+    """Representing square."""
+
+
+
+    def __init__(self, size):
+        """Initialize a square
+
+
+        Args:
+            Size: The new sqaure size."""
+        self.__size = size
